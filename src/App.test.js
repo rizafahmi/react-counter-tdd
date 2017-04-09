cb26af4b275898407ee56a25c0779c64674e6f6a
Riza Fahmi <rizafahmi@gmail.com>
2017-04-09 09:39:26 +0700

    Downgrade to react v15.4.2

--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,10 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import App from './App';
+import React from 'react'
+import ReactDOM from 'react-dom'
+import { shallow } from 'enzyme'
+
+import App from './App'
 
 it('renders without crashing', () => {
-  const div = document.createElement('div');
-  ReactDOM.render(<App />, div);
-});
+  const app = shallow(<App />)
+  expect(app).toBeDefined()
+})
