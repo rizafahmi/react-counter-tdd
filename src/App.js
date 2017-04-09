b25f28c69753f4885bcdabfa1d67534292b54bff
Riza Fahmi <rizafahmi@gmail.com>
2017-04-09 09:53:18 +0700

    Passing test #2: renders Counter

--- src/App.js
+++ src/App.js
@@ -1,12 +1,13 @@
 import React, { Component } from 'react'
-import logo from './logo.svg'
-import './App.css'
+
+import Counter from './components/Counter.js'
 
 class App extends Component {
   render () {
     return (
       <div>
         <h1>React Counter</h1>
+        <Counter />
       </div>
     )
   }
---
+++ src/components/Counter.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const Counter = (props) => {
+  return (
+    <h2>0</h2>
+  )
+}
+
+export default Counter
