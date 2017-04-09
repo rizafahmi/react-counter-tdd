b7fe49d95cc1ffcb72361e9e2a2dd902d7e24b8f
Riza Fahmi <rizafahmi@gmail.com>
2017-04-09 09:44:03 +0700

    Write test case for App's components

--- src/App.test.js
+++ src/App.test.js
@@ -8,3 +8,10 @@ it('renders without crashing', () => {
   const app = shallow(<App />)
   expect(app).toBeDefined()
 })
+
+describe('Renders <App /> components', () => {
+  it('should render title component')
+  it('should render counter component')
+  it('should render increment button')
+  it('should render decrement button')
+})
