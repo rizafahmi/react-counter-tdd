3d00c9ffb5c0011fed50e354cc6456006cdc4dbe
Riza Fahmi <rizafahmi@gmail.com>
2017-04-09 09:54:39 +0700

    Refactor test

--- src/App.test.js
+++ src/App.test.js
@@ -10,16 +10,20 @@ it('renders without crashing', () => {
 })
 
 describe('Renders <App /> components', () => {
+  let app
+  beforeEach(() => {
+    app = shallow(<App />)
+  })
   it('should render title component', () => {
-    const app = shallow(<App />)
     const actual = app.find('h1').text()
     expect(actual).toEqual('React Counter')
   })
   it('should render counter component', () => {
-    const app = shallow(<App />)
     const actual = app.find('Counter')
     expect(actual.length).toEqual(1)
   })
-  it('should render increment button')
+  it('should render increment button', () => {
+
+  })
   it('should render decrement button')
 })
