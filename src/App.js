c8d618169317823d0017c18f72af9544775a32c6
Riza Fahmi <rizafahmi@gmail.com>
2017-04-09 12:29:56 +0700

    Pass test about initial state

--- src/App.js
+++ src/App.js
@@ -3,6 +3,12 @@ import React, { Component } from 'react'
 import Counter from './components/Counter.js'
 
 class App extends Component {
+  constructor (props) {
+    super(props)
+    this.state = {
+      counter: 0
+    }
+  }
   render () {
     return (
       <div>
